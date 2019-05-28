8c7d9ae0e52bc904e819c988fae59cdbfa5d6b36
李元秋 <yuanqiu.li@wz-inc.com>
2019-05-28 13:58:32 +0800

    auto update readme.md variables

---
+++ scripts/update-readme.js
@@ -0,0 +1,21 @@
+const { readFile, writeFile } = require('fs');
+
+readFile('./package.json', (err, buf) => {
+  if (err) {
+    throw err;
+  }
+  const { version } = JSON.parse(buf.toString());
+
+  readFile('./README.md', (err1, buf1) => {
+    if (err1) {
+      throw err1;
+    }
+    const text = buf1.toString().replace(/\${version}/g, version);
+
+    writeFile('./README.md', text, err2 => {
+      if (err2) {
+        throw err2;
+      }
+    });
+  });
+});
